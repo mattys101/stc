4187fe88db83d419e7fb7a3781c9b248dcf9722d
mattys101 <30459857+mattys101@users.noreply.github.com>
2017-10-11 16:27:45 +1030

    Preparation for implementing the Java wrapper classes.

---
+++ src/test/java/st/redline/kernel/TestJava.java
@@ -0,0 +1,13 @@
+
+package st.redline.kernel;
+
+import junit.framework.Assert;
+
+public class TestJava {
+
+    public void testTest() {
+
+        Assert.assertEquals(true, false);
+    }
+
+}
---
+++ src/test/java/st/redline/kernel/TestJavaClass.java
@@ -0,0 +1,13 @@
+
+package st.redline.kernel;
+
+import junit.framework.Assert;
+
+public class TestJavaClass {
+
+    public void testTestStatic() {
+
+        Assert.assertEquals("Not working ha!", false, true);
+    }
+
+}
